716229350e5c3f569dea017b15f401e1c276c910
zpy <zpyssr@gmail.com>
2019-06-20 14:41:59 +0800

    控制器继承代码分离.

---
+++ pinyougou2_parent/pinyougou2_manager_web/src/main/webapp/js/controller/baseController.js
@@ -0,0 +1,39 @@
+app.controller('baseController', function ($scope) {
+    //分页控件配置
+    //currentPage 当前页
+    //totalPage 总记录数
+    //itemsPerPage 每页记录数
+    //perPageOptions 分页选项
+    //onChange 当页码重新变更后,自动触发的方法
+    $scope.paginationConf = {
+        currentPage: 1,
+        totalPage: 10,
+        itemsPerPage: 10,
+        perPageOptions: [10, 20, 30, 40, 50],
+        onChange: function () {
+            $scope.reloadList();
+        }
+    };
+
+    //刷新列表
+    $scope.reloadList = function () {
+        // $scope.findPage($scope.paginationConf.currentPage, $scope.paginationConf.itemsPerPage)
+        $scope.search($scope.paginationConf.currentPage, $scope.paginationConf.itemsPerPage)
+    };
+
+    //删除品牌
+    //思路整理
+    //$event 是一个圆,指的是input本身
+    $scope.selectIds = [];//用户勾选的id集合
+    $scope.updateSelection = function ($event, id) {
+        //只有被选中的时候才添加到ids集合中
+        if ($event.target.checked) {
+            $scope.selectIds.push(id);//push向集合中添加元素
+        } else {
+            //如果没被选中则删除集合中的元素
+            var index = $scope.selectIds.indexOf(id);//查找值的位置
+            $scope.selectIds.splice(index, 1);//参数1:移除的位置 参数2:移除的个数
+        }
+    };
+
+});
--- pinyougou2_parent/pinyougou2_manager_web/src/main/webapp/js/controller/brandController.js
+++ pinyougou2_parent/pinyougou2_manager_web/src/main/webapp/js/controller/brandController.js
@@ -1,4 +1,8 @@
-app.controller("brandController", function ($scope, $http, brandService) {
+app.controller("brandController", function ($scope, $http, $controller, brandService) {
+    //继承
+    //伪继承 {$scope: $scope} 意思是把baseController和brandController中的$scope链接起来
+    //$controller 也需要传递,需要在参数中注入
+    $controller('baseController', {$scope: $scope});
     $scope.findAll = function () {
         brandService.findAll().success(
             function (response) {
@@ -6,26 +10,7 @@ app.controller("brandController", function ($scope, $http, brandService) {
             }
         );
     };
-    //分页控件配置
-    //currentPage 当前页
-    //totalPage 总记录数
-    //itemsPerPage 每页记录数
-    //perPageOptions 分页选项
-    //onChange 当页码重新变更后,自动触发的方法
-    $scope.paginationConf = {
-        currentPage: 1,
-        totalPage: 10,
-        itemsPerPage: 10,
-        perPageOptions: [10, 20, 30, 40, 50],
-        onChange: function () {
-            $scope.reloadList();
-        }
-    };
-    //刷新列表
-    $scope.reloadList = function () {
-        // $scope.findPage($scope.paginationConf.currentPage, $scope.paginationConf.itemsPerPage)
-        $scope.search($scope.paginationConf.currentPage, $scope.paginationConf.itemsPerPage)
-    };
+
     //分页
     $scope.findPage = function (page, rows) {
         brandService.findPage(page, rows).success(
@@ -62,20 +47,7 @@ app.controller("brandController", function ($scope, $http, brandService) {
             }
         );
     };
-    //删除品牌
-    //思路整理
-    //$event 是一个圆,指的是input本身
-    $scope.selectIds = [];//用户勾选的id集合
-    $scope.updateSelection = function ($event, id) {
-        //只有被选中的时候才添加到ids集合中
-        if ($event.target.checked) {
-            $scope.selectIds.push(id);//push向集合中添加元素
-        } else {
-            //如果没被选中则删除集合中的元素
-            var index = $scope.selectIds.indexOf(id);//查找值的位置
-            $scope.selectIds.splice(index, 1);//参数1:移除的位置 参数2:移除的个数
-        }
-    };
+
     //用户删除
     $scope.dele = function () {
         brandService.dele($scope.selectIds).success(
