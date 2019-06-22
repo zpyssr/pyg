e28188ea24ab7016eeea2409e98275f34749c88c
zpy <zpyssr@gmail.com>
2019-06-22 20:26:52 +0800

    完成了商家待审核列表

--- pinyougou2_parent/pinyougou2_manager_web/src/main/webapp/js/controller/sellerController.js
+++ pinyougou2_parent/pinyougou2_manager_web/src/main/webapp/js/controller/sellerController.js
@@ -4,13 +4,13 @@ app.controller('sellerController' ,function($scope,$controller   ,sellerService)
 	$controller('baseController',{$scope:$scope});//继承
 	
     //读取列表数据绑定到表单中  
-	$scope.findAll=function(){
-		sellerService.findAll().success(
-			function(response){
-				$scope.list=response;
-			}			
-		);
-	}    
+    $scope.findAll = function () {
+        sellerService.findAll().success(
+            function (response) {
+                $scope.list = response;
+            }
+        );
+    };
 	
 	//分页
 	$scope.findPage=function(page,rows){			
