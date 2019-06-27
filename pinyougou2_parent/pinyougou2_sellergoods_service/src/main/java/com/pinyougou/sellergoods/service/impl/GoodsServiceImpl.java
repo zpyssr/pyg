52027e8abf52a2946a8906ff605a0c9160c1fe5b
zpy <zpyssr@gmail.com>
2019-06-27 19:09:39 +0800

    第六天:6.3 是否启用规格后端实现

--- pinyougou2_parent/pinyougou2_sellergoods_service/src/main/java/com/pinyougou/sellergoods/service/impl/GoodsServiceImpl.java
+++ pinyougou2_parent/pinyougou2_sellergoods_service/src/main/java/com/pinyougou/sellergoods/service/impl/GoodsServiceImpl.java
@@ -151,36 +151,54 @@ public class GoodsServiceImpl implements GoodsService {
         goodsMapper.insert(goods.getGoods());//插入商品表
         goods.getGoodsDesc().setGoodsId(goods.getGoods().getId());
         goodsDescMapper.insert(goods.getGoodsDesc());//插入商品扩展数据
-        for (TbItem item : goods.getItemList()) {
-            //标题
-            String title = goods.getGoods().getGoodsName();
-            Map<String, Object> specMap = JSON.parseObject(item.getSpec());
-            for (String key : specMap.keySet()) {
-                title += "" + specMap.get(key);
+        //判断是否使用规格
+        if ("1".equals(goods.getGoods().getIsEnableSpec())) {
+            for (TbItem item : goods.getItemList()) {
+                //标题
+                String title = goods.getGoods().getGoodsName();
+                Map<String, Object> specMap = JSON.parseObject(item.getSpec());
+                for (String key : specMap.keySet()) {
+                    title += "" + specMap.get(key);
+                }
+                item.setTitle(title);
+                setItemValues(goods, item);
+                itemMapper.insert(item);
             }
-            item.setTitle(title);
-            item.setGoodsId(goods.getGoods().getId());//商品SPU编号
-            item.setSellerId(goods.getGoods().getSellerId());//商家编号
-            item.setCategoryid(goods.getGoods().getCategory3Id());//商品分类编号(3级)
-            item.setCreateTime(new Date());//创建日期
-            item.setUpdateTime(new Date());//修改日期
-            //品牌名称
-            TbBrand brand = brandMapper.selectByPrimaryKey(goods.getGoods().getBrandId());
-            item.setBrand(brand.getName());
-            //分类名称
-            TbItemCat itemCat = itemCatMapper.selectByPrimaryKey(goods.getGoods().getCategory3Id());
-            item.setCategory(itemCat.getName());
-            //商家名称
-            TbSeller seller = sellerMapper.selectByPrimaryKey(goods.getGoods().getSellerId());
-            item.setSeller(seller.getNickName());
-            //图片地址(取spu的第一个图片)
-            List<Map> imageList = JSON.parseArray(goods.getGoodsDesc().getItemImages(), Map.class);
-            if (imageList.size() > 0) {
-                item.setImage((String) imageList.get(0).get("url"));
-            }
+
+        } else {
+            TbItem item = new TbItem();
+            item.setTitle(goods.getGoods().getGoodsName());//商品KPU+规格描述串作为SKU名称
+            item.setPrice(goods.getGoods().getPrice());//价格
+            item.setStatus("1");//状态
+            item.setIsDefault("1");//是否默认
+            item.setSpec("{}");
+            setItemValues(goods, item);
             itemMapper.insert(item);
         }
 
+
+    }
+
+    private void setItemValues(Goods goods, TbItem item) {
+        item.setGoodsId(goods.getGoods().getId());//商品SPU编号
+        item.setSellerId(goods.getGoods().getSellerId());//商家编号
+        item.setCategoryid(goods.getGoods().getCategory3Id());//商品分类编号(3级)
+        item.setCreateTime(new Date());//创建日期
+        item.setUpdateTime(new Date());//修改日期
+        //品牌名称
+        TbBrand brand = brandMapper.selectByPrimaryKey(goods.getGoods().getBrandId());
+        item.setBrand(brand.getName());
+        //分类名称
+        TbItemCat itemCat = itemCatMapper.selectByPrimaryKey(goods.getGoods().getCategory3Id());
+        item.setCategory(itemCat.getName());
+        //商家名称
+        TbSeller seller = sellerMapper.selectByPrimaryKey(goods.getGoods().getSellerId());
+        item.setSeller(seller.getNickName());
+        //图片地址(取spu的第一个图片)
+        List<Map> imageList = JSON.parseArray(goods.getGoodsDesc().getItemImages(), Map.class);
+        if (imageList.size() > 0) {
+            item.setImage((String) imageList.get(0).get("url"));
+        }
     }
 
 
