b86eeaf697e45436f9a04b9ed169e731bba6d886
zpy <zpyssr@gmail.com>
2019-07-03 19:19:28 +0800

    第十一天: 过滤查询 价格区间过滤

--- pinyougou2_parent/pinyougou2_search_web/src/main/webapp/js/controller/searchController.js
+++ pinyougou2_parent/pinyougou2_search_web/src/main/webapp/js/controller/searchController.js
@@ -1,7 +1,7 @@
 app.controller('searchController', function ($scope, searchService) {
 
     //定义搜索对象的结构 category : 商品分类
-    $scope.searchMap = {'keywords': '', 'category': '', 'brand': '', 'spec': {}};
+    $scope.searchMap = {'keywords': '', 'category': '', 'brand': '', 'spec': {}, 'price': ''};
 
     //搜索
     $scope.search = function () {
@@ -14,7 +14,7 @@ app.controller('searchController', function ($scope, searchService) {
 
     //添加搜索项,改变searchMap的值
     $scope.addSearchItem = function (key, value) {
-        if (key == 'category' || key == 'brand') {//如果用户点击的是分类或者品牌
+        if (key == 'category' || key == 'brand' || key == 'price') {//如果用户点击的是分类或者品牌
             $scope.searchMap[key] = value;
         } else {//规格是用户点击的
             $scope.searchMap.spec[key] = value;
